8c36bdb89142a2aba7b81afe77dab5e21b83345c
C15HOT <wvxp@mail.ru>
2022-08-12 20:10:38 +0300

    init

--- app/libs/postgres/handlers.py
+++ app/libs/postgres/handlers.py
@@ -1,58 +1,142 @@
+from datetime import datetime
 from typing import List
 
 from fastapi import HTTPException, status
 from pydantic import UUID4
-from sqlalchemy import create_engine
+
+from sqlalchemy.future import select
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.sql import exists
+from sqlalchemy.sql.elements import and_
+
 from app.libs.postgres.models import Event
 from app.schemas.calendar_schemas import EventsSchema
 from app.settings import get_settings
 
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy import update, delete
+
 settings = get_settings()
 
 connection = f'{settings.postgres_settings.postgres_server}{settings.postgres_settings.postgres_db}'
+engine = create_async_engine(connection, echo=True)
+async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
-def insert_event(event: EventsSchema,
+async def chek_event_exists_and_owner_rights(event_id: UUID4, owner_id: UUID4) -> Event:
+    async with async_session() as session:
+        event = await session.get(Event, event_id)
+        if event:
+            if event.owner_id == owner_id:
+                return event
+            else:
+                raise HTTPException(status.HTTP_403_FORBIDDEN, 'Forbidden')
+        else:
+            raise HTTPException(status.HTTP_404_NOT_FOUND, detail='Event not found')
+
+
+async def insert_event(event: EventsSchema,
                  owner_id: UUID4):
-    engine = create_engine(connection)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    new_event = Event(id=event.id,
-                      title=event.title,
-                      description=event.description,
-                      from_datetime=event.from_datetime,
-                      to_datetime=event.to_datetime,
-                      location=event.location,
-                      is_online_event=event.is_online_event,
-                      photo_uri=event.photo_uri,
-                      repeat_mode=event.repeat_mode.value,
-                      repeat_days=event.repeat_days,
-                      repeat_end=event.repeat_end,
-                      source=event.source,
-                      owner_id=owner_id,
-                      )
-    try:
-        session.add(new_event)
-        session.commit()
-    finally:
-        session.close()
+    async with async_session() as session:
+
+        new_event = Event(id=event.id,
+                          title=event.title,
+                          description=event.description,
+                          from_datetime=event.from_datetime.replace(tzinfo=None),
+                          to_datetime=event.to_datetime.replace(tzinfo=None),
+                          location=event.location,
+                          is_online_event=event.is_online_event,
+                          photo_uri=event.photo_uri,
+                          repeat_mode=event.repeat_mode.value,
+                          repeat_days=event.repeat_days,
+                          repeat_end=event.repeat_end,
+                          source=event.source,
+                          owner_id=owner_id,
+                          )
+        async with session.begin():
+            session.add(new_event)
+        await session.commit()
 
 
-def get_event(event_id: UUID4) -> Event:
-    engine = create_engine(connection)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    event = session.query(Event).filter(Event.id == event_id).first()
-    session.close()
+
+async def get_event(event_id: UUID4, user_id: UUID4) -> Event:
+    event = await chek_event_exists_and_owner_rights(event_id=event_id, owner_id=user_id)
     return event
 
+async def get_events(owner_id) -> List[Event]:
+    async with async_session() as session:
+        stmt = select(Event).where(Event.owner_id==owner_id)
+        result = await session.execute(stmt)
+        events = result.scalars().all()
+        return events
 
-def get_events(owner_id) -> List[Event]:
-    engine = create_engine(connection)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    events = session.query(Event).filter(Event.owner_id == owner_id).all()
-    session.close()
-    return events
+
+async def update_event(event_id: UUID4, event: EventsSchema, owner_id: UUID4):
+    async with async_session() as session:
+        await chek_event_exists_and_owner_rights(event_id=event_id, owner_id=owner_id)
+        stmt = update(Event).where(Event.id == event_id, Event.owner_id == owner_id).values(title=event.title,
+                              description=event.description,
+                              from_datetime=event.from_datetime.replace(tzinfo=None),
+                              edited_at=datetime.now(),
+                              to_datetime=event.to_datetime.replace(tzinfo=None),
+                              location=event.location,
+                              is_online_event=event.is_online_event,
+                              photo_uri=event.photo_uri,
+                              repeat_mode=event.repeat_mode.value,
+                              repeat_days=event.repeat_days,
+                              repeat_end=event.repeat_end,
+                              source=event.source)
+
+        await session.execute(stmt)
+        await session.commit()
+
+
+async def delete_event(event_id: UUID4, owner_id: UUID4):
+    async with async_session() as session:
+        await chek_event_exists_and_owner_rights(event_id=event_id, owner_id=owner_id)
+        stmt = delete(Event).where(Event.id == event_id, Event.owner_id == owner_id)
+        await session.execute(stmt)
+        await session.commit()
+
+
+
+async def find_events_by_filters(owner_id, title, from_datetime, to_datetime, location):
+    async with async_session() as session:
+
+        if owner_id:
+            owner_id = str(owner_id)
+            stmt = select(Event).filter(Event.owner_id == owner_id)
+        if title:
+            stmt = select(Event).filter(Event.title.like(f'%{title}%'))
+        if from_datetime:
+            stmt = select(Event).filter(Event.from_datetime >= from_datetime)
+        if to_datetime:
+            stmt = select(Event).filter(Event.to_datetime <= to_datetime)
+        if location:
+            stmt = select(Event).filter(Event.location.like(f'%{location}%'))
+
+        stmt = select(Event).filter(and_(Event.title.like(f'%{title}%'),
+                                         Event.owner_id == str(owner_id)))
+        result = await session.execute(stmt)
+        events = result.scalars().all()
+        return events
+
+
+# async def accept_event(event_id: UUID4, user_id: UUID4):
+#     async with async_session() as session:
+#         event_user = Event_User(id=event.id,
+#                           title=event.title,
+#                           description=event.description,
+#                           from_datetime=event.from_datetime.replace(tzinfo=None),
+#                           to_datetime=event.to_datetime.replace(tzinfo=None),
+#                           location=event.location,
+#                           is_online_event=event.is_online_event,
+#                           photo_uri=event.photo_uri,
+#                           repeat_mode=event.repeat_mode.value,
+#                           repeat_days=event.repeat_days,
+#                           repeat_end=event.repeat_end,
+#                           source=event.source,
+#                           owner_id=owner_id,
+#                           )
+#         async with session.begin():
+#             session.add(new_event)
+#         await session.commit()
--- app/routers/events_router.py
+++ app/routers/events_router.py
@@ -3,17 +3,18 @@ from app.schemas.calendar_schemas import *
 from app.libs.auth import auth_required
 from fastapi import status, HTTPException
 from app.settings import get_settings
-from app.libs.postgres.handlers import insert_event, get_event, get_events
+from app.libs.postgres.handlers import insert_event, get_event, get_events, update_event, delete_event, \
+    find_events_by_filters
 
 events_router = APIRouter(tags=['Events'])
 settings = get_settings()
 
 
 @events_router.post('/add_event/', summary='Add event calendar. Access: all authenticated users')
-async def add_event(event: EventsSchema,
+async def add_event_route(event: EventsSchema,
                     owner_id: UUID4 = Depends(auth_required)):
     try:
-        insert_event(event=event, owner_id=owner_id)
+        await insert_event(event=event, owner_id=owner_id)
     except BaseException as exc:
         print(exc)
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Failed to write event to database')
@@ -21,28 +22,55 @@ async def add_event(event: EventsSchema,
 
 
 @events_router.get('/get_event/{evend_id}', summary='Get event calendar')
-async def add_event(event_id: UUID4,
+async def get_event_route(event_id: UUID4,
                     user_id: UUID4 = Depends(auth_required)):
-    try:
-        event = get_event(event_id=event_id)
+    event = await get_event(event_id=event_id, user_id=user_id)
+    return event
 
-    except BaseException as exc:
-        print(exc)
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Failed to read event from database')
-
-    if event.owner_id == user_id:
-        return event
-    else:
-        raise HTTPException(status.HTTP_403_FORBIDDEN, detail='Not allowed')
 
 
 @events_router.get('/all_user_events/', summary='Get events calendar')
-async def add_event(owner_id: UUID4 = Depends(auth_required)):
-    try:
-        events = get_events(owner_id=owner_id)
+async def get_events_route(owner_id: UUID4 = Depends(auth_required)):
+        events = await get_events(owner_id=owner_id)
         return events
+
+
+
+
+@events_router.put('/update_event/{evend_id}', summary='Update event')
+async def update_event_route(event_id: UUID4,
+                       event: EventsSchema,
+                       owner_id: UUID4 = Depends(auth_required)):
+    try:
+        updated_event = await update_event(event_id=event_id, owner_id=owner_id,
+                     event=event)
+        return updated_event
     except BaseException as exc:
         print(exc)
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Failed to read events from database')
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Failed to update event')
 
 
+@events_router.delete('/delete_event/{event_id}', summary='Delete event')
+async def delete_event_route(event_id: UUID4,
+                       owner_id: UUID4 = Depends(auth_required)):
+    await delete_event(event_id=event_id, owner_id=owner_id)
+    return status.HTTP_200_OK
+
+
+
+@events_router.get('/get_events_by_filter/', summary='Find events by filter')
+async def find_events_by_filter_route(title: str = None,
+                                      from_datetime: datetime = None,
+                                      to_datetime: datetime = None,
+                                      location: str = None,
+                                      user_id: UUID4 = None,
+                                      owner_id: UUID4 = Depends(auth_required),
+                                      ):
+
+    events = await find_events_by_filters(owner_id=user_id,
+                                          title=title,
+                                          from_datetime=from_datetime,
+                                          to_datetime=to_datetime,
+                                          location=location)
+
+    return events
